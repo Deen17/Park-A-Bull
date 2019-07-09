0597c1b050629b639d904f3132e27a708b81bc41
Adil Riazudeen <adilriazudeen@gmail.com>
2019-07-09 00:07:20 -0400

    Update config.js

--- ParkABull/db/config.js
+++ ParkABull/db/config.js
@@ -2,5 +2,5 @@
 module.exports = {
     url: "https://parkabull.localtunnel.me/",
     //localUrl: "http://10.100.3.204:8000/"
-    localUrl: "https://2c9a7c9f.ngrok.io/"
+    localUrl: "https://580ad7db.ngrok.io/"
 }
