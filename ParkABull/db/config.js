7e753953d2f22af38138cc5a776148f96c3c250d
Deen17 <adilriazudeen@gmail.com>
2019-10-24 14:32:18 -0400

    deleting some things
    deleting some things

--- ParkABull/db/config.js
+++ ParkABull/db/config.js
@@ -1,6 +1,4 @@
 //module.exports = config;
 module.exports = {
-    url: "https://parkabull.localtunnel.me/",
-    //localUrl: "http://10.100.3.204:8000/"
     localUrl: "https://9cdd6e31.ngrok.io/"
 }
